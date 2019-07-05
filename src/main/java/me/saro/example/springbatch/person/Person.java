41176467c8276f5fca6310db51b3554da618f6aa
PARK Yong Seo <j@saro.me>
2019-07-05 18:32:35 +0900

    add unit person and resources

---
+++ src/main/java/me/saro/example/springbatch/person/Person.java
@@ -0,0 +1,9 @@
+package me.saro.example.springbatch.person;
+
+import lombok.*;
+
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter @ToString
+public class Person {
+    private String lastName;
+    private String firstName;
+}
